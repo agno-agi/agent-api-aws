0d126447add2ed5ad112dd992ac2585964fe9050
ysolanky <yash@phidata.com>
2025-05-11 22:55:41 -0400

    update

--- workspace/settings.py
+++ workspace/settings.py
@@ -24,7 +24,7 @@ ws_settings = WorkspaceSettings(
     # 'Name:tag' for the image
     image_name="agent-api",
     # Build images locally
-    build_images=True,
+    build_images=False,
     # Push images after building
     push_images=False,
     # Skip cache when building images
